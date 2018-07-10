f83de4950556a1601e5781be4e45e3ad7c707212
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-07-10 17:51:25 +0200

    create Rabbit class

---
+++ src/main/java/Rabbit.java
@@ -0,0 +1,18 @@
+public class Rabbit {
+
+    private int age;
+    private boolean isFemale;
+
+    public Rabbit(boolean isFemale) {
+        age = 0;
+        this.isFemale = isFemale;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isFemale() {
+        return isFemale;
+    }
+}
