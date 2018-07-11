5322d07d2a8dfaaa72608b6463a35001c92013d3
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-07-11 19:59:11 +0200

    refactor SimulationSPOJStyle (comments removed)

--- src/main/java/SimulationSPOJStyle.java
+++ src/main/java/SimulationSPOJStyle.java
@@ -6,30 +6,25 @@ public class SimulationSPOJStyle {
         long[] maleGenerations = new long[97];
         int months = 4;
 
-        //initial conditions
         femaleGenerations[4] = femaleCount;
         maleGenerations[4] = maleCount;
         long population = maleCount + femaleCount;
 
-        //breeding loop
         long newFemaleGeneration;
         long newMaleGeneration;
         while (population < limit) {
             newFemaleGeneration = 0;
             newMaleGeneration = 0;
 
-            //breeding of mature females
             for (int i = 4; i < 97; i++) {
                 newFemaleGeneration += femaleGenerations[i] * 14;
                 newMaleGeneration += femaleGenerations[i] * 5;
             }
 
-            //aging process
             for (int i = 95; i >= 0; i--) {
                 femaleGenerations[i + 1] = femaleGenerations[i];
                 maleGenerations[i + 1] = maleGenerations[i];
             }
-            //adding new generations to arrays
             femaleGenerations[0] = newFemaleGeneration;
             maleGenerations[0] = newMaleGeneration;
 
